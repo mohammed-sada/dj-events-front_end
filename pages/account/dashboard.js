74a6c8e1d5c5398134a8f495937c3aa89a83aa10
mohammed-sada <mohammedabusada94@example.com>
2021-07-01 18:32:54 +0300

    fetching user events for dashboard

--- pages/account/dashboard.js
+++ pages/account/dashboard.js
@@ -1,9 +1,28 @@
 import Layout from "@/components/Layout";
+import { API_URL } from "@/config/index";
+import { parseCookies } from "@/helpers/index";
+import axios from "axios";
 
-export default function dashboard() {
+export default function dashboard({ events }) {
+  console.log(events);
   return (
     <Layout>
       <h1>Dashboard</h1>
     </Layout>
   );
+}
+
+export async function getServerSideProps({ req }) {
+  const { token } = parseCookies(req);
+  console.log(token);
+
+  const data = await fetch(`${API_URL}/events/me`, {
+    method: "GET",
+    headers: {
+      Authorization: `bearer ${token}`,
+    },
+  });
+  const events = await data.json();
+
+  return { props: { events } };
 }
